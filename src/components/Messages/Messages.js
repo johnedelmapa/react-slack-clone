8d45a2fad4c0880ad71819f2586ded69940072fc
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-04-29 16:18:08 +0800

    Create Upload Photo

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,13 +1,83 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
 
-export class Messages extends Component {
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+import Message from "./Message";
+
+class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    messages: [],
+    messagesLoading: true,
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
+
+  componentDidMount(){
+    const { channel, user } = this.state;
+
+    if(channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  componentWillUnmount(){
+    this.removeListeners();
+  }
+  
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  }
+
+  removeListeners = () => {
+    this.state.messagesRef.off();
+  }
+
+  addMessageListener = channelID => {
+    let loadedMessages = []
+    this.state.messagesRef.child(channelID).on('child_added', snap => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+    })
+  }
+
+  displayMessages = messages =>
+  messages.length > 0 &&
+  messages.map(message => (
+    <Message
+      key={message.timestamp}
+      message={message}
+      user={this.state.user}
+    />
+  ));
+
+
   render() {
+    const { messagesRef, messages, channel, user } = this.state;
+
     return (
-      <div>
-        Messages
-      </div>
-    )
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </React.Fragment>
+    );
   }
 }
 
-export default Messages
+export default Messages;
